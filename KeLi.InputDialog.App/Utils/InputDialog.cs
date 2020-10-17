42c74a9d503418e51253903183c1390263743943
KeLi <keliwork@outlook.com>
2020-10-17 18:30:08 +0800

    Updates email.

--- KeLi.InputDialog.App/Utils/InputDialog.cs
+++ KeLi.InputDialog.App/Utils/InputDialog.cs
@@ -32,7 +32,7 @@
      |  |                                                    |  |  |     |         |      |
      |  |                                                    |  |  |/----|`---=    |      |
      |  |              Author: KeLi                          |  |  |     |         |      |
-     |  |              Email: kelistudy@163.com              |  |  |     |         |      |
+     |  |              Email: kelicto@protonmail.com         |  |  |     |         |      |
      |  |              Creation Time: 11/23/2019 03:22:13 PM |  |  |     |         |      |
      |  | C:\>_                                              |  |  |     | -==----'|      |
      |  |                                                    |  |  |   ,/|==== ooo |      ;
